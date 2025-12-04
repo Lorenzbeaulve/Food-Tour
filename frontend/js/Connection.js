0af7f5e7874e95ad1e087943398f8792f7e0b013
Lorenzo Maria Sarica <116087294+Lorenzbeaulve@users.noreply.github.com>
2025-12-04 11:50:43 +0100

    estensione di quanto detto prima

--- frontend/js/Connection.js
+++ frontend/js/Connection.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'Food-TourDB'
+    database: 'FoodTour'
 });
 
 module.exports = pool;
--- frontend/js/server.js
+++ frontend/js/server.js
@@ -6,3 +6,69 @@ let pool = require("./conn");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/connect', async (req, res) => {
+    try{
+        console.log(req.body);
+        res.json({success: true, msg: 'connected to the database'});
+    }
+    catch (err) {
+        res.json({success: false, msg: 'connection error'});
+    }
+})
+
+// Endpoint per eseguire query
+app.post('/query', async (req, res) => {
+    try {
+        const [rows] = await pool.query(req.body.sql);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const query = "SELECT username, password FROM utenti WHERE username = ?";
+        const [rows] = await pool.execute(query, [username]);
+
+        if (rows.length === 0) {
+            return res.status(401).json({ success: false, msg: "Username o password errati" });
+        }
+
+        const user = rows[0];
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            console.log("Passwords don't match");
+            return res.status(401).json({ success: false, msg: "Username o password errati" });
+        }
+
+        res.json({ success: true, username: user.username });
+        console.log("Successfully logged in");
+    } catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+});
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try{
+        const pswd = await bcrypt.hash(password, 10);
+        const query = "INSERT INTO utenti (username, email , password) VALUES (?, ?, ?)";
+        await pool.execute(query, [username, email, pswd]);
+        console.log("success");
+        res.json({success: true, msg: 'successfully registered'});
+    }
+    catch (err) {
+        console.log(err.message);
+        res.status(500).json({ success: false, error: err.message });
+    }
+})
+
+app.listen(3000, () => console.log('Server in ascolto su http://localhost:3000'));
